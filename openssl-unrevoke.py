a0fc72aa03e91d3f797447fab61b636dd806151c
Aleksa Majkić <aleksamcode@gmail.com>
2022-07-31 15:08:11 +0200

    Unnecessary code removed

--- openssl-unrevoke.py
+++ openssl-unrevoke.py
@@ -10,9 +10,8 @@ lines = file.readlines()
 for i in range(len(lines)):
     if serial_num in lines[i]:
         split_line = [value.strip() for value in lines[i].split('\t')]
-        split_line[0] = 'V'
         lines[i] = \
-            f"{split_line[0]}\t{split_line[2].split(',')[0]}\t\t{split_line[3]}\t{split_line[4]}\t{split_line[5]}"
+            f"V\t{split_line[2].split(',')[0]}\t\t{split_line[3]}\t{split_line[4]}\t{split_line[5]}"
         break
 
 file.close()
